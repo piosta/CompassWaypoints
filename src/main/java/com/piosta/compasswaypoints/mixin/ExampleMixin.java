e0524d3e8a602e7b005bd38f2d2d41837ea6a22f
Piotr Stańczyk <piotr.jerzy.stanczyk@hotmail.com>
2019-05-09 23:08:50 +0200

    Mod run

--- src/main/java/com/piosta/compasswaypoints/mixin/ExampleMixin.java
+++ src/main/java/com/piosta/compasswaypoints/mixin/ExampleMixin.java
@@ -1,6 +1,6 @@
 package com.piosta.compasswaypoints.mixin;
 
-import net.minecraft.client.MinecraftClient;
+//import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.hud.InGameHud;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -10,8 +10,17 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 //@Mixin(MinecraftClient.class)
 @Mixin(InGameHud.class)
 public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "init()V")
-	private void init(CallbackInfo info) {
-		System.out.println("This line is printed by an example mod mixin!");
+	@Inject(at = @At("RETURN"), method = "draw(F)V")
+	public void draw(float delta, CallbackInfo info)
+	{
+//		MapGUI map = MapGUI.instance;
+//		if (map != null)
+//		{
+//			map.draw();
+//		}
+//
+//		GlStateManager.disableDepthTest();
+//		WayPointRenderer.render(delta);
+//		GlStateManager.enableDepthTest();
 	}
 }
