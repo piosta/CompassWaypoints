c33cec32ae98cd38686001b33881ebe907d43d77
Piotr Stańczyk <piotr.jerzy.stanczyk@hotmail.com>
2019-11-17 21:22:03 +0100

    Stub project without any mods implementation

---
+++ src/main/java/net/waypoints/compass/CompassWaipoints.java
@@ -0,0 +1,13 @@
+package net.waypoints.compass;
+
+import net.fabricmc.api.ModInitializer;
+
+public class CompassWaipoints implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
